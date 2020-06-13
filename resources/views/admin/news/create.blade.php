bb3e015a93ef234c99bd5bc3f272680b03055297
西山 <tatsuyasuukatuyou@gmail.com>
2020-06-13 11:17:30 +0000

    現状データ送付

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,15 +1,4 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>Myニュース作成画面</h1>
-    </body>
-</html>
 {{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
 
